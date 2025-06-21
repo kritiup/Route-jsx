e9da5187b68d9930ba1c16166ac2c01c87df0088
Kriti Upadhyay <kritiu54@gmail.com>
2025-06-21 23:40:14 +0545

    editing app.jsx

--- learning-route/src/App.jsx
+++ learning-route/src/App.jsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+ function App(){
+  return(
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element= {<Home />} />
+          <Route path="/home" element = {<Home />}/>
+          <Route path="/about" element = {<About />}/>
+          <Route path="/contact" element = {<Contact />}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-}
-
+ }
  export default App
